921f689113ff6361c6c6888ba185653784158c39
Shizhu Liang <shizhuliang@Shizhus-MacBook-Air.local>
2014-07-30 16:54:31 -0700

    changed assets compile serve static assets to be true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,12 @@ Blogger::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -44,6 +42,11 @@ Blogger::Application.configure do
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
+  
+  
+  config.assets.compile = true
+
+  config.serve_static_assets = true
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
