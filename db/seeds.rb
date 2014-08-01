a85e1410ae1b7457b5192a275e1f4278d533108f
Shizhu Liang <shizhuliang@shizhus-air.gateway.2wire.net>
2014-08-01 00:09:15 -0700

    fixed font color and added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,116 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+random_words = 
+  %w(adducing
+dulcified
+ennead
+prereview
+hesitated
+vesture
+outpiping
+unbathed
+retinite
+chromolithograph
+sauerkraut
+panama
+maurice
+prereckon
+rescheduled
+subrotund
+mesoblast
+permissive
+pinite
+hue
+nonglare
+nonfusibility
+impanelling
+redecided
+rebrew
+airth
+foetation
+sphenoidal
+valiance
+exclosure
+inclination
+zee
+transdermic
+prebelief
+verrocchio
+nonpercipience
+scand
+casually
+anneal
+cotised
+epicalyx
+unnoticed
+floreat
+robbery
+distally
+phidippides
+reptiloid
+peaceably
+precharging
+inconspicuousness
+irreducible
+concessively
+recoupable
+mortifyingly
+lankiest
+epiclike
+minuend
+apprehender
+venturing
+forthcomingness
+nonimpressionistic
+bellmead
+antirational
+substandardization
+outawed
+unsafetied
+stieglitz
+pindaric
+unbudgeted
+expensive
+jabberingly
+preparedly
+multiview
+nonpenetrating
+harness
+paraglyphas
+racialism
+egotistic
+letch
+pseudoconfessional
+waister
+subjudge
+barfly
+shiftingness
+myelencephalon
+reendow
+auditor
+lunge
+polymyxin
+cassini
+relost
+squeakingly
+coding
+enamelware
+telescoping
+pergamus
+unpargeted
+fortress
+affreight
+overcommon)
+
+f = File.open("lib/seed_data/randomName.txt", "r")
+f.each_line do |line|
+  newUser = User.create!(username: line, password: "password")
+  newUser.posts.create!(title: ("My name is " + "#{newUser.username}"), content: random_words.shuffle.join(" "))
+end
+f.close
+
+User.each.posts.create!(title: ("Second post and my name is" + "#{newUser.username}"), content: random_words.shuffle.join(" "))
+User.each.posts.create!(title: ("Third post and my name is" + "#{newUser.username}"), content: random_words.shuffle.join(" "))
+User.each.posts.create!(title: ("Fourth post and my name is" + "#{newUser.username}"), content: random_words.shuffle.join(" "))
